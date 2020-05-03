07a0c6a3751b4fdbb4e573c198645e17c6dee731
Abin Joshy <abinjkakkanattu.97@gmail.com>
2020-05-03 22:01:37 +0530

    update_04/04/20

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 #May 3 2020: (845108) 
 	Initial Commit: Backend-service
 	Service Interation with Database(Basic CURD Operations)
+#May 4 2020: (845108)
+	Update in Backend-service
+	- added basic logic maping
--- backend-service/src/main/java/com/cts/training/backendservice/controller/UserController.java
+++ backend-service/src/main/java/com/cts/training/backendservice/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.cts.training.backendservice.controller;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cts.training.backendservice.models.Users;
 import com.cts.training.backendservice.repo.UserRepo;
+import com.cts.training.backendservice.service.UserService;
 
 @RestController
 public class UserController {
@@ -21,38 +28,55 @@ public class UserController {
 	@Autowired
 	UserRepo userrepo;
 	
-	@GetMapping("/")
-	public String start() {
-		return "Hello";
-	}
+	@Autowired
+	UserService userservice;
+	
 	
 	@GetMapping("/users")
 	public List<Users> findAll() {
-		return userrepo.findAll();
+		return userservice.getAll();
 	}
 	
 	@GetMapping("/users/{id}")
 	public Users findOne(@PathVariable int id) {
-		Optional<Users> usr = userrepo.findById(id);
-		Users us = usr.get();
+		Users us = userservice.getOne(id);
 		return us;
 	}
 	
 	@PostMapping("/users")
 	public Users save(@RequestBody Users usr) {
-		Users us = userrepo.save(usr);
+		Users us = userservice.insert(usr);
 		return us;
 	}
 	
 	@DeleteMapping("/users/{id}")
 	public void delete(@PathVariable int id) {
-		userrepo.deleteById(id);
+		userservice.remove(id);
 	}
 	
 	@PutMapping("/users")
 	public Users update(@RequestBody Users usr) {
-		Users us = userrepo.save(usr);
+		Users us = userservice.alter(usr);
 		return us;
 	}
+	@GetMapping("/users/login")
+	public ResponseEntity<?> login(HttpServletRequest request){
+		String credentials =null;
+		String authorization = request.getHeader("Authorization");
+		if (authorization != null && authorization.startsWith("Basic")) {
+			String base64Credentials = authorization.substring("Basic".length()).trim();
+		    byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
+		    credentials = new String(credDecoded, StandardCharsets.UTF_8);
+		}
+		try {
+			Users user = userservice.getUserByUsernameAndPassword(credentials.split(":")[0],credentials.split(":")[1]);
+			
+			return new ResponseEntity<Users>(user,HttpStatus.OK);
+		} catch (Exception e ) {
+			System.out.println(e.getStackTrace());
+			return new ResponseEntity<String>("No user found",HttpStatus.NOT_FOUND);
+		}
+	}
+	
 
 }
